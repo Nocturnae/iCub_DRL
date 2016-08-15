a34e99560c21b38a1b62db597b2ffaef0758d9e8
Zeynep Akkalyoncu <zeynep.akkalyoncu@metu.edu.tr>
2016-08-15 11:32:10 +0300

    Added light source

--- BlenderGenerator/generator.py
+++ BlenderGenerator/generator.py
@@ -1,11 +1,34 @@
 import bpy
 
-delete_list = [item.name for item in bpy.data.objects if item.type == "MESH"]
+def clear_scene():
+	"""
+	delete all existing objects
+	"""
+	delete_list = [item.name for item in bpy.data.objects if item.type == "MESH" or item.type == "LAMP"]
 
-for obj in delete_list:
-    bpy.data.objects[obj].select = True
+	for obj in delete_list:
+	    bpy.data.objects[obj].select = True
 
-bpy.objs.objects.delete()
+	bpy.ops.object.delete()
 
-for item in bpy.data.meshes:
-    bpy.data.meshes.remove(item)
+	for item in bpy.data.meshes:
+	    bpy.data.meshes.remove(item)
+
+def add_light():
+	"""
+	add light source
+	"""
+	scene = bpy.context.scene
+	lamp_data = bpy.data.lamps.new(name="Lamp", type='POINT')
+	lamp_obj = bpy.data.objects.new(name="Lamp", object_data=lamp_data)
+	scene.objects.link(lamp_obj)
+	lamp_obj.location = (5.0, 5.0, 5.0)
+	lamp_obj.select = True
+	scene.objects.active = lamp_obj
+
+def run():
+	clear_scene()
+	add_light()
+
+if __name__ == "__main__":
+	run()
